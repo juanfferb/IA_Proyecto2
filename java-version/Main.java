b1396f69f8d335cf70c5b632a5351da3c09b705e
joalvarado27 <146464326+joalvarado27@users.noreply.github.com>
2025-03-31 23:08:41 -0500

    Actualizacion Main

--- java-version/Main.java
+++ java-version/Main.java
@@ -6,13 +6,13 @@ public class Main {
         Resolucion kb = new Resolucion(); // Creamos una base de conocimiento
 
         // Agregamos cláusulas (hechos y reglas)
-        kb.agregarClausula(new Clausula(Set.of(new Literal("Hombre", "a"))));
-        kb.agregarClausula(new Clausula(Set.of(new Literal("Pompeyano", "a"))));
+        kb.agregarClausula(new Clausula(Set.of(new Literal("Hombre", "Marco"))));
+        kb.agregarClausula(new Clausula(Set.of(new Literal("Pompeyano", "Marco"))));
         kb.agregarClausula(new Clausula(Set.of(new Literal("Romano", "a"), new Literal("-Pompeyano", "a"))));
-        kb.agregarClausula(new Clausula(Set.of(new Literal("Gobernante", "c"))));
-        kb.agregarClausula(new Clausula(Set.of(new Literal("Leal", "a", "c"), new Literal("Odia", "a", "c"), new Literal("-Romano", "a"))));
+        kb.agregarClausula(new Clausula(Set.of(new Literal("Gobernante", "Cesar"))));
+        kb.agregarClausula(new Clausula(Set.of(new Literal("Leal", "a", "Cesar"), new Literal("Odia", "a", "Cesar"), new Literal("-Romano", "a"))));
         kb.agregarClausula(new Clausula(Set.of(new Literal("-Leal", "a", "b"), new Literal("-Hombre", "a"), new Literal("-IntentaAsesinar", "a", "b"), new Literal("-Gobernante", "b"))));
-        kb.agregarClausula(new Clausula(Set.of(new Literal("IntentaAsesinar", "a", "c"))));
+        kb.agregarClausula(new Clausula(Set.of(new Literal("IntentaAsesinar", "Marco", "Cesar"))));
 
                 // Mostrar base de conocimiento actual
         System.out.println("Base de conocimiento inicial:");
@@ -21,11 +21,11 @@ public class Main {
         System.out.println("--------------------------");
 
         // Declaramos la conjetura a verificar
-        List<Literal> conjetura = List.of(new Literal("Odia", "a", "c"));
+        List<Literal> conjetura = List.of(new Literal("Odia", "Marco", "Cesar"));
         kb.negarConjetura(conjetura); // Se niega la conjetura
 
         // Se sustituyen las variables por nombres reales
-        Map<String, String> variables = kb.unificarVariables("Marco", "Cesar", "Cesar");
+        Map<String, String> variables = kb.unificarVariables("Marco", "Cesar");
         kb.sustituirVariables(variables);
 
         // Mostrar la base de conocimiento después de la sustitución
